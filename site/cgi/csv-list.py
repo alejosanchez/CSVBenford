793fc92f0f7e682c2650ada1f91d1456c90fa683
alejosanchez <lists@alejosanchez.com>
2013-08-05 19:39:04 -0300

    List existing datasets

---
+++ site/cgi/csv-list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Give back the columns of a CSV and the in
+# http://www.tutorialspoint.com/python/python_cgi_programming.htm
+
+
+from   os      import listdir
+from   os.path import isfile, join
+from   fnmatch import fnmatch
+import cgitb
+
+CSV_DIR = '../csv/' # CSV upload directory
+
+cgitb.enable()      # pretty debugging
+
+csv_files = [ f for f in listdir(CSV_DIR) if fnmatch(f, '*.csv') ]
+
+print '''\
+Status: 200\r
+Content-Type: application/json;charset=UTF-8\r
+\r
+{ "csvFiles" : [%s] }\r
+''' % ( '"' + '","'.join(csv_files) + '"', )
+
